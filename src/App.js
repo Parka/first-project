257df4f0b86098e9fe7d9bc420e95582319ca510
Alejandra Merel <merelesale@gmail.com>
2022-10-07 12:52:24 -0300

    cambio de texto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Parka
+          Aprendiendo JavaScri con Parka, somos conejillos de india
         </a>
       </header>
     </div>
