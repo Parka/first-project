0d6734066a082a7a00cffa31210312096b63fbc1
Parka <franciscocasanovaferro@gmail.com>
2022-10-15 18:31:06 -0300

    Add VideoCardSmall component

---
+++ src/components/VideoCardSmall/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+const VideoCardSmall = () => {
+  return (
+    <span className={styles.main}>
+      VideoCardSmall
+    </span>
+  )
+};
+
+export default VideoCardSmall;
