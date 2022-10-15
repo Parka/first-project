1ca9a51e9d120769f521fe7ad90cadbea5be1f46
Alejandra Merel <merelesale@gmail.com>
2022-10-15 19:00:37 -0300

    add structure to Sidebarcard

--- src/components/SidebarCard/index.js
+++ src/components/SidebarCard/index.js
@@ -1,7 +1,15 @@
 import styles from "./styles.module.css";
 const SidebarCard = () => {
-    return <span className={styles.main}
-    >"SidebarCard"</span>
+  return (
+    <div className={styles.main} >
+      <div className={styles.image}></div>
+      <div className={styles.body}>
+        <div className={styles.header}> Why is Uber selling its autonomous-vehicle division? </div>
+        <div className={styles.text}> Self-driving cars were meant to be its future </div>
+      </div>
+
+    </div>
+  )
 
 };
 
