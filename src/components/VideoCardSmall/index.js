06a346b843bb0b937e43fa6617ebe717095d27c1
Parka <franciscocasanovaferro@gmail.com>
2022-10-15 19:00:42 -0300

    add structure to VideoCardSmall

--- src/components/VideoCardSmall/index.js
+++ src/components/VideoCardSmall/index.js
@@ -1,11 +1,15 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 const VideoCardSmall = () => {
   return (
-    <span className={styles.main}>
-      VideoCardSmall
-    </span>
-  )
+    <div className={styles.main}>
+      <div className={styles.image}></div>
+      <div className={styles.body}>
+        <div className={styles.header}>Hancock: vaccines 80% effective at preventing hospitalisations in over-80s</div>
+        <div className={styles.text}></div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCardSmall;
