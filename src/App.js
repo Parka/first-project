f979b5fc08ba8e1233dcf51242457c91861280cd
Alejandra Merel <merelesale@gmail.com>
2022-10-18 20:57:26 -0300

    Finish components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import SidebarCard from "./components/SidebarCard";
 function App() {
   return (
     <div>
-      <SidebarCard/>
+      <SidebarCard
+      tag= "Future"
+      header="Why is Uber selling its autonomous-vehicle division?"
+      text = "Self-driving cars were meant to be its futures"  
+      date = "25 Feb 2021" />
     </div>
   );
 }
--- src/components/SidebarCard/index.js
+++ src/components/SidebarCard/index.js
@@ -1,11 +1,15 @@
 import styles from "./styles.module.css";
-const SidebarCard = () => {
+const SidebarCard = (props) => {
   return (
-    <div className={styles.main} >
+    
+    <div className={styles.main} > 
+    <div className={styles.tag}> {props.tag}</div>
       <div className={styles.image}></div>
       <div className={styles.body}>
-        <div className={styles.header}> Why is Uber selling its autonomous-vehicle division? </div>
-        <div className={styles.text}> Self-driving cars were meant to be its future </div>
+        <div className={styles.header}> {props.header} </div>
+        <div className={styles.text}> {props.text} </div>
+        <div className={styles.date}>{props.date} </div>
+        
       </div>
 
     </div>
