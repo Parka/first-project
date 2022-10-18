dbf9c24ca60ea71086c70c1f19d45fcbcaa83934
Nicolás Corcos <nicolascorcos@gmail.com>
2022-10-18 20:44:17 -0300

    Details in Footerpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Footerpage from './components/Footerpage'
 
 function App() {
   return (
--- src/components/Footerpage/index.js
+++ src/components/Footerpage/index.js
@@ -1,11 +1,15 @@
 import styles from './styles.module.css';
 
-const Footetpage = () => {
+const Footerpage = () => {
   return (
     <div className={styles.main}>
-      
+      <div className={styles.container}>
+        <div className={styles.logo}></div>
+        <div className={styles.copy}>© 2021 Moneta News & Media Hungary Zrt. All rights reserved.</div>
+      </div>
+
     </div>
   )
 };
 
-export default HeadlineCard;
+export default Footerpage;
