6a2281f3e0e2303b3f27018b0f3e75b775d378a4
Nicolás Corcos <nicolascorcos@gmail.com>
2022-10-18 21:01:35 -0300

    Add mapsite in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,18 @@ const Footer = () => {
     <div className={styles.main}>
       <div className={styles.container}>
         <div className={styles.logo}></div>
+        <div className={styles.sitemap}>
+          <ul className={styles.column}>
+            <li className={styles.options}>Home</li>
+          </ul>
+          <ul className={styles.column}>
+            <li className={styles.options}>Home</li>
+          </ul>
+          <ul className={styles.column}>
+            <li className={styles.options}>Home</li>
+          </ul>
+          
+        </div>
         <div className={styles.copy}>© 2021 Moneta News & Media Hungary Zrt. All rights reserved.</div>
       </div>
 
