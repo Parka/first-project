0575c45c8a0ea3f17faae6323fd084660128527e
Parka <franciscocasanovaferro@gmail.com>
2022-10-18 21:04:50 -0300

    finish component

--- src/components/VideoCardSmall/index.js
+++ src/components/VideoCardSmall/index.js
@@ -3,11 +3,11 @@ import styles from "./styles.module.css";
 const VideoCardSmall = () => {
   return (
     <div className={styles.main}>
-      <div className={styles.image}></div>
-      <div className={styles.body}>
-        <div className={styles.header}>Hancock: vaccines 80% effective at preventing hospitalisations in over-80s</div>
-        <div className={styles.text}></div>
+      <div className={styles.image}>
+        <div className={styles.play}/>
       </div>
+      <div className={styles.header}>Hancock: vaccines 80% effective at preventing hospitalisations in over-80s</div>
+      <div className={styles.date}>23 Feb 2021</div>
     </div>
   );
 };
